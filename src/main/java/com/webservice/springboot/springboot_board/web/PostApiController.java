33a167257ef6e7961631550c8b8aba474f9ed531
yujin <yujin201318@naver.com>
2023-07-27 21:06:12 +0900

    스프링부트와 AWS로 혼자 구현하는 웹서비스 2~5.2 실습코드

---
+++ src/main/java/com/webservice/springboot/springboot_board/web/PostApiController.java
@@ -0,0 +1,35 @@
+package com.webservice.springboot.springboot_board.web;
+
+import com.webservice.springboot.springboot_board.service.posts.PostsService;
+import com.webservice.springboot.springboot_board.web.dto.PostsResponseDto;
+import com.webservice.springboot.springboot_board.web.dto.PostsSaveRequestDto;
+import com.webservice.springboot.springboot_board.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostApiController {
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id,@RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id){
+        return postsService.findById(id);
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id){
+        postsService.delete(id);
+        return id;
+    }
+}
