aab6a343fab127cf19f1f5674403f112ef290b63
yujin <yujin201318@naver.com>
2023-08-13 21:22:31 +0900

    스프링부트와 AWS로 혼자 구현하는 웹서비스 5장까지의 실습코드

---
+++ src/main/java/com/webservice/springboot/springboot_board/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.webservice.springboot.springboot_board.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
