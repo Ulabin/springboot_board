329d618c79b078b851f6d570ead9145c9c19fec8
yujin <yujin201318@naver.com>
2023-08-13 21:19:05 +0900

    스프링부트와 AWS로 혼자 구현하는 웹서비스 5장까지의 실습코드

--- src/main/java/com/webservice/springboot/springboot_board/web/IndexController.java
+++ src/main/java/com/webservice/springboot/springboot_board/web/IndexController.java
@@ -1,21 +1,36 @@
 package com.webservice.springboot.springboot_board.web;
 
+import com.webservice.springboot.springboot_board.config.auth.LoginUser;
+import com.webservice.springboot.springboot_board.config.auth.dto.SessionUser;
 import com.webservice.springboot.springboot_board.service.posts.PostsService;
 import com.webservice.springboot.springboot_board.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
+@Slf4j
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
+//    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
+        log.info("index");
+
         model.addAttribute("posts", postsService.findAllDesc());
+//        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user!=null){
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
